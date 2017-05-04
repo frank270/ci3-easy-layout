5acabb9310e2413a51f754bfc0edc1fe564e7930
Frank Wang <frank270@gmail.com>
2017-05-04 12:55:27 +0800

    Update MY_Controller.php

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,17 +1,37 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
-
+/**
+ * 
+ */
 class MY_Controller extends CI_Controller
 {
+    /**
+     * [$data description]
+     * @var array
+     */
     protected $data = array();
+    /**
+     * [$template description]
+     * @var string
+     */
     protected $template = 'master_view';
+    /**
+     * [__construct description]
+     */
     public function __construct()
     {
         parent::__construct();
-        $this->data['pagetitle'] = 'My CodeIgniter App';
+        //$this->data['pagetitle'] = 'My CodeIgniter App';
     }
-
+    /**
+     * [render description]
+     * @param  [type]  $the_view [description]
+     * @param  [type]  $data     [description]
+     * @param  boolean $return   [description]
+     * @return [type]            [description]
+     */
     protected function render($the_view = null, $data = null, $return = false)
     {
+        $layout_data = $data;
         if (is_null($this->template)) {
             if ($return) {
                 return $this->load->view($the_view, $data, true);
@@ -20,17 +40,32 @@ class MY_Controller extends CI_Controller
             }
         } else {
             if ($return) {
-                $data['the_view_content'] = (is_null($the_view)) ? '' : $this->load->view($the_view, $data, true);
-                $output = $this->load->view('templates/' . $this->template . '_view', $data, true);
+                $layout_data['the_view_content'] = (is_null($the_view)) ? '' : $this->load->view($the_view, $data, true);
+                $output = $this->load->view($this->template, $layout_data, true);
                 return $output;
             } else {
-                $data['the_view_content'] = (is_null($the_view)) ? '' : $this->load->view($the_view, $data, true);
-                $this->load->view('templates/' . $this->template, $data);
+                $layout_data['the_view_content'] = (is_null($the_view)) ? '' : $this->load->view($the_view, $data, true);
+                $this->load->view( $this->template, $layout_data);
             }
         }
     }
+    /**
+     * [setLayout description]
+     * @param [type] $layout [description]
+     */
     protected function setLayout($layout)
     {
         $this->template = $layout;
+    }
+    /**
+     * [dd description]
+     * @param  [type] $obj [description]
+     * @return [type]      [description]
+     */
+    protected function dd($obj)
+    {
+        echo "<pre>";
+        print_r($obj);
+        echo "</pre>";
     }
 }
